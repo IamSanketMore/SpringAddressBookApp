95ee0aea44a7ba696753b6aac9c3d43e12ec0d6b
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-26 16:33:02 +0530

    [UPDATED]S3-UC2:- Use Lombook libraray For Logging

--- SpringAddreeBookApp/src/main/java/com/ideaclan/addressbook/model/AddressBookData.java
+++ SpringAddreeBookApp/src/main/java/com/ideaclan/addressbook/model/AddressBookData.java
@@ -1,7 +1,9 @@
 package com.ideaclan.addressbook.model;
 
 import com.ideaclan.addressbook.DTO.AddressBookDTO;
+import lombok.Data;
 
+@Data
 public class AddressBookData
 {
     private int addressBookId;
@@ -26,59 +28,4 @@ public class AddressBookData
     }
 
 
-    public int getAddressBookId() {
-        return addressBookId;
-    }
-
-    public void setAddressBookId(int addressBookId) {
-        this.addressBookId = addressBookId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
 }
