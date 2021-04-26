f6a40035bd64be82630db05e656e7c2e7b410e9b
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-26 20:20:19 +0530

    [ADDED]S4-UC2:- Provide User Friendly Error Response in case validation fails

---
+++ SpringAddreeBookApp/src/main/java/com/ideaclan/addressbook/exceptions/AddressBookExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.ideaclan.addressbook.exceptions;
+
+import com.ideaclan.addressbook.DTO.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class AddressBookExceptionHandler
+{
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handlerMethodArgNotValidException(MethodArgumentNotValidException exception){
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errormsg = errorList.stream().map(objerr-> objerr.getDefaultMessage()).collect(Collectors.toList());
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing rest request", errormsg);
+        return new ResponseEntity<ResponseDTO>(responseDTO , HttpStatus.BAD_REQUEST);
+
+        }
+
+}
