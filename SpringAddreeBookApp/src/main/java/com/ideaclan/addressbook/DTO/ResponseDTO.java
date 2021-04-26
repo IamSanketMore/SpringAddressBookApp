317f63d02034659a9ce149e8bd850ca22230df18
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-26 15:08:26 +0530

    [UPDATED]S3-UC1:- Use Lombook libraray to generate getters ans setters for DTO

--- SpringAddreeBookApp/src/main/java/com/ideaclan/addressbook/DTO/ResponseDTO.java
+++ SpringAddreeBookApp/src/main/java/com/ideaclan/addressbook/DTO/ResponseDTO.java
@@ -1,5 +1,8 @@
 package com.ideaclan.addressbook.DTO;
 
+import lombok.Data;
+
+@Data
 public class ResponseDTO
 {
     private String message;
@@ -7,22 +10,6 @@ public class ResponseDTO
 
     public ResponseDTO(String message, Object data) {
         this.message = message;
-        this.data = data;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
         this.data = data;
     }
 }
