d91ffe2675af3d091384b97ffcf7e266baed53dd
IamSanketMore <sanketmore.999.m@gmail.com>
2021-04-26 19:10:58 +0530

    [UPDATED]S3-UC4:- DataBase setting as Enviroument Variables

--- SpringAddreeBookApp/src/main/java/com/ideaclan/addressbook/AddressbookApplication.java
+++ SpringAddreeBookApp/src/main/java/com/ideaclan/addressbook/AddressbookApplication.java
@@ -12,6 +12,8 @@ public class AddressbookApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(AddressbookApplication.class, args);
         log.info("Address Book app started in {} environment..", context.getEnvironment().getProperty("environment"));
+        log.info("ADDRESS BOOK DB User is = {} ",context.getEnvironment().getProperty("spring.datasource.username"));
+        log.info("ADDRESS BOOK DB password is = {} ",context.getEnvironment().getProperty("spring.datasource.password"));
     }
 
 }
